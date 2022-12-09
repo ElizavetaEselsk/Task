fad2badbd7929db987add0b080d86a9bb53acdb5
ElizavetaEselsk <120220112+ElizavetaEselsk@users.noreply.github.com>
2022-12-09 23:43:29 +0300

    Task_OOP_number15
    Task_OOP_number15

---
+++ Task/src/geometry/Point.java
@@ -0,0 +1,51 @@
+package geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Point implements Geometry{
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "x=" + x + ", y=" + y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+
+
+    public List<Point> getPoints() {
+        List<Point> l = new ArrayList<>();
+        l.add(this);
+        return l;
+    }
+}
